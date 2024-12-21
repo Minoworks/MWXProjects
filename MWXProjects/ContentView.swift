7684fa88ba71844cd53722addb4bc0e555ec8ced
Mino <minoworks@gmail.com>
2024-12-21 14:17:41 +0900

    Initial Commit

---
+++ MWXProjects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MWXProjects
+//
+//  Created by Mino Kim on 12/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MWXProjects/MWXProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MWXProjectsApp.swift
+//  MWXProjects
+//
+//  Created by Mino Kim on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MWXProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
